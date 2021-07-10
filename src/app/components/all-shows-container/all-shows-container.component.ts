4def4e625796b01941023689be709509c092fb18
Slavko Kekic <skekich@gmail.com>
2021-07-10 18:39:49 +0200

    add show description data

--- src/app/components/all-shows-container/all-shows-container.component.ts
+++ src/app/components/all-shows-container/all-shows-container.component.ts
@@ -12,31 +12,36 @@ export class AllShowsContainerComponent implements OnInit {
 	rawShowsData: Array<IShows> = [
 		{
 			title: 'The Expanse',
-			description: 'This is a show!',
+			description:
+				'A thriller set two hundred years in the future, The Expanse follows the case of a missing young woman who brings a hardened detective and a rogue ships captain together in a race across the solar system to expose the greatest conspiracy in human history.',
 			average_rating: [4, 5, 5, 4],
 			image_url: '../assets/images/expanse.jpg',
 		},
 		{
-			title: 'Star Trek',
-			description: 'This is a show!',
+			title: 'Star Trek: The Next Generation',
+			description:
+				'Captain Jean-Luc Picard leads a new generation of Starfleet officers in the Enterprise NCC 1701-D spacecraft to seek out new planet and life forms in space.',
 			average_rating: [5, 5, 5, 4],
 			image_url: '../assets/images/star_trek.jpg',
 		},
 		{
 			title: 'Battlestar Galactica',
-			description: 'This is a show!',
+			description:
+				'A group of humans aboard a battleship, Battlestar Galactica, are forced to abandon their planet after being attacked by Cylons. They try to evade the Cylons while searching for their true home, Earth.',
 			average_rating: [4, 4, 5, 4],
 			image_url: '../assets/images/bsg.jpg',
 		},
 		{
 			title: 'X Files',
-			description: 'This is a show!',
+			description:
+				'Conspiracy theorist Fox Mulder and realist Dana Scully pull out all the stops as FBI special agents to investigate and get to the bottom of inexplicable paranormal cases.',
 			average_rating: [5, 4, 5, 4],
 			image_url: '../assets/images/x_files.jpg',
 		},
 		{
 			title: 'The Clone wars',
-			description: 'This is a show!',
+			description:
+				'As multiple star systems get involved in the Clone Wars, the Jedi Knights struggle to keep the peace and defeat the droid army of the Separatists. Meanwhile, an old threat slowly reveals its presence.',
 			average_rating: [3, 4, 5, 4],
 			image_url: '../assets/images/clone_wars.jpg',
 		},
