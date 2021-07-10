00e55ae3959bf2c7fcb8d83e1d0f3c3055f2d0c7
Slavko Kekic <skekich@gmail.com>
2021-07-10 17:00:04 +0200

    add main layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,10 +10,20 @@ import { ShowCardComponent } from './components/show-card/show-card.component';
 import { ShowListComponent } from './components/show-list/show-list.component';
 import { RatingComponent } from './components/rating/rating.component';
 import { AllShowsContainerComponent } from './components/all-shows-container/all-shows-container.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 
 @NgModule({
-	declarations: [AppComponent, ShowCardComponent, ShowListComponent, RatingComponent, AllShowsContainerComponent],
-	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatButtonModule, MatIconModule],
+	declarations: [
+		AppComponent,
+		ShowCardComponent,
+		ShowListComponent,
+		RatingComponent,
+		AllShowsContainerComponent,
+		SidenavComponent,
+		MainLayoutComponent,
+	],
+	imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatIconModule, MatSidenavModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
