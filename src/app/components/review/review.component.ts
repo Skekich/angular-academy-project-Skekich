5f32641e9fa695f71770d0b302c2d4a1ca75e898
Slavko Kekic <skekich@gmail.com>
2021-08-01 14:07:10 +0200

    adjust review component styling remove media query

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,4 +1,8 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ILayout } from 'src/app/interfaces/layout.interface';
 
 @Component({
 	selector: 'app-review',
@@ -11,4 +15,16 @@ export class ReviewComponent {
 	@Input() comment: string;
 	@Input() email: string;
 	@Input() imageUrl: string;
+
+	public layout$: Observable<ILayout>;
+
+	constructor(breakpointsObserver: BreakpointObserver) {
+		this.layout$ = breakpointsObserver.observe([Breakpoints.Small, Breakpoints.XSmall]).pipe(
+			map(({ matches }) => {
+				return {
+					isSmall: matches,
+				};
+			})
+		);
+	}
 }
